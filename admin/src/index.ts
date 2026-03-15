286c35cb1bacb0a8c53651cbb670ddd8a660366b
mmanikandap0512 <manikandaprabhu0512@gmail.com>
2026-03-15 23:16:24 +0530

    Update CORS configuration

--- admin/src/index.ts
+++ admin/src/index.ts
@@ -9,10 +9,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173",
-      "https://tomato-app-frontend.vercel.app/",
-    ],
+    origin: ["http://localhost:5173", "https://tomato-app-frontend.vercel.app"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],

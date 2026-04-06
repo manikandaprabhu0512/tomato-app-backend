a361a5d11d522a809904d0a1c60fa52af9c0deee
mmanikandap0512 <manikandaprabhu0512@gmail.com>
2026-04-06 23:52:39 +0530

    refractor: add test or dummy menuitems data

--- restaurant/seed.js
+++ restaurant/seed.js
@@ -1,22 +1,27 @@
 import mongoose from "mongoose";
 import Restaurant from "./dist/models/Restaurant.js";
+import MenuItems from "./dist/models/MenuItems.js";
 import dotenv from "dotenv";
 
 import fs from "fs";
 
-const data = JSON.parse(
+const restaurants_data = JSON.parse(
   fs.readFileSync(new URL("./restaurants_seed.json", import.meta.url)),
+);
+
+const menuitems_data = JSON.parse(
+  fs.readFileSync(new URL("./menuitems_seed.json", import.meta.url)),
 );
 
 dotenv.config();
 
 const MONGO_URI = `${process.env.MONGO_URI}Zomato_Clone`;
 
-async function seed() {
+async function seed_restaurants() {
   try {
     await mongoose.connect(MONGO_URI);
 
-    await Restaurant.insertMany(data);
+    await Restaurant.insertMany(restaurants_data);
 
     console.log("Seeded 100 restaurants ✓");
 
@@ -27,4 +32,19 @@ async function seed() {
   }
 }
 
-seed();
+async function seed_menuitems() {
+  try {
+    await mongoose.connect(MONGO_URI);
+
+    await MenuItems.insertMany(menuitems_data);
+
+    console.log("Seeded menuitems ✓");
+
+    await mongoose.disconnect();
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+}
+
+seed_menuitems();
